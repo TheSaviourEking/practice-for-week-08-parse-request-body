3ad0c42fb190154c976f853701c33baafd4517ea
TheSaviourEking <saviour.eking@outlook.com>
2024-01-16 07:37:23 +0100

    fix all the required processes in the parseBody algorithm

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,50 @@
 function firstStep(input) {
-  // Your code here
+    // Your code here
+    return input.replace("&", " ").split(" ");
 }
+let pInput = "username=azure+green&password=password%21";
+let res = fifthStep(fourthStep(thirdStep(secondStep(firstStep("username=azure+green&password=password%21")))));
+console.log(parseBody(pInput));
 
 function secondStep(input) {
-  // Your code here
+    // Your code here
+    return input.map(pair => pair.split('='));
 }
 
 function thirdStep(input) {
-  // Your code here
+    // Your code here
+    return input.map(pair => {
+	return pair.map(value => value.replace(/\+/g, ' '));
+    });
 }
 
 function fourthStep(input) {
-  // Your code here
+    // Your code here
+    return input.map(pair => pair.map(value => decodeURIComponent(value)))
 }
 
 function fifthStep(input) {
-  // Your code here
+    // Your code here
+    // console.log(input);
+    // return input.reduce((accumulator, currentValue) => {
+    // });
+    const res = {};
+    input.forEach(pairs => {
+	res[pairs[0]] = pairs[1];
+    });
+    return res;
 }
 
 function parseBody(str) {
-  // Your code here
+    // Your code here
+    const reqBody = str;
+    const keyValuePairs = firstStep(reqBody);
+    const arr2DkeyValuePairs = secondStep(keyValuePairs);
+    const replacePlusSigns = thirdStep(arr2DkeyValuePairs);
+    const decodedValues = fourthStep(replacePlusSigns);
+    const object = fifthStep(decodedValues);
+    
+    return object;
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -6,6 +6,14 @@ let server;
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
 // Your code here
+const http = require('http');
+
+server = http.createServer((req, res) => {
+    console.log(req.method, req.url);
+});
+
+const PORT = 5000;
+server.listen(PORT, () => console.log('Successfully started the server on port', PORT));
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
